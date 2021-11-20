fbf564b27fd99eac0eb7353268f2507b43c5d5db
Trapti Rahangdale <imtrapti1410@gmail.com>
2021-11-20 12:27:19 +0530

    tablist is working with keyboard navigation

--- public/js/index.js
+++ public/js/index.js
@@ -10,13 +10,55 @@ function initNavigation(){
 }
 
 function initTabList(){
-	const keyCodeLeft = 37;
-	const keyCodeRight = 39;
-
 	const tabList = document.querySelector('[role="tablist"]');
-	tabList.addEventListener('keydown', (e) => {
+	const tabs = tabList.querySelectorAll('[role="tab"]');
+	const firstTab = tabs[0];
+	const lastTab = tabs[tabs.length - 1];
 
+	function markTabAndPanelUnselected(el) {
+		el.setAttribute('tabindex', "-1");
+		el.setAttribute('aria-selected', false);
+		const panel = document.getElementById(el.getAttribute('aria-controls'));
+		panel.setAttribute("hidden", true);
+		const photo = document.getElementById(panel.getAttribute("data-photo"));
+		photo.setAttribute("hidden", true);
+	}
+
+	function markTabAndPanelSelected(el) {
+		el.setAttribute('tabindex', "0");
+		el.setAttribute('aria-selected', "true");
+		const panel = document.getElementById(el.getAttribute('aria-controls'));
+		panel.removeAttribute("hidden");
+		const photo = document.getElementById(panel.getAttribute("data-photo"));
+		photo.removeAttribute("hidden");
+		el.focus();
+	}
+
+	tabList.addEventListener('keydown', (e) => {
+		const currentEl = e.target;
+
+		let nextEl = null;
+		switch (e.key) {
+			case "ArrowRight":
+				nextEl = currentEl.nextSibling || firstTab;
+				break;
+
+			case "ArrowLeft":
+				nextEl = currentEl.previousSibling || lastTab;
+				break;
+
+			default:
+				return;
+		}
+
+		if (!nextEl) {
+			return;
+		}
+
+		markTabAndPanelUnselected(currentEl);
+		markTabAndPanelSelected(nextEl);
 	});
 }
 
 initNavigation();
+initTabList();
