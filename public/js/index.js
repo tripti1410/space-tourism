4bb6def5b8e74d62186bf0ea7c054ac9ff6102bb
Trapti Rahangdale <imtrapti1410@gmail.com>
2021-11-20 11:22:21 +0530

    WIP

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const el = document.querySelector(".mobile-nav-toggle");
-    el.addEventListener("click", function (event){
-        const el = event.target;
-        const toggled = !(el.getAttribute("aria-expanded") === "true");
-        el.setAttribute("aria-expanded", toggled);
-    });
-});
+function initNavigation(){
+	document.addEventListener("DOMContentLoaded", function () {
+		const el = document.querySelector(".mobile-nav-toggle");
+		el.addEventListener("click", function (event){
+			const el = event.target;
+			const toggled = !(el.getAttribute("aria-expanded") === "true");
+			el.setAttribute("aria-expanded", toggled);
+		});
+	});
+}
+
+function initTabList(){
+	const keyCodeLeft = 37;
+	const keyCodeRight = 39;
+
+	const tabList = document.querySelector('[role="tablist"]');
+	tabList.addEventListener('keydown', (e) => {
+
+	});
+}
+
+initNavigation();
