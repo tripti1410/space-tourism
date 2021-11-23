f22ff0a26ca4c48a710de757d40761fbcebe6bc0
Trapti Rahangdale <imtrapti1410@gmail.com>
2021-11-23 11:30:06 +0530

    added slider navigation

--- public/js/index.js
+++ public/js/index.js
@@ -75,15 +75,15 @@ function initTabList(){
 
 	tabList.addEventListener('click', (e) => {
 		const currentEl = e.target;
-		const buttonEl = currentEl.closest('button')
-		if (!buttonEl) {
+		const tabEl = currentEl.closest('[role="tab"]')
+		if (!tabEl) {
 			return;
 		}
 
 		const selectedTabEl = tabs.filter(t => t.getAttribute('aria-selected') === 'true')[0];
 
 		markTabAndPanelUnselected(selectedTabEl);
-		markTabAndPanelSelected(buttonEl);
+		markTabAndPanelSelected(tabEl);
 	});
 
 }
@@ -96,14 +96,22 @@ function initDotSlider () {
 	const dots = Array.from(dotList.querySelectorAll('button'));
 
 	dotList.addEventListener('click', (e) => {
+		const clickedDot = e.target.closest('button');
+		if(!clickedDot){
+			return;
+		}
+
 		const selectedDot = dots.find(dot => dot.getAttribute('aria-selected') === "true")
 		const selectedDotSlide = document.getElementById(selectedDot.getAttribute('data-slide'))
+		const selectedDotImage = document.getElementById(selectedDotSlide.getAttribute('data-image'))
 		selectedDotSlide.setAttribute('hidden', "true");
+		selectedDotImage.setAttribute('hidden', "true");
 		selectedDot.setAttribute('aria-selected', "false")
 
-		const clickedDot = e.target
 		const clickedDotSlide = document.getElementById(clickedDot.getAttribute('data-slide'));
+		const clickedDotImage = document.getElementById(clickedDotSlide.getAttribute('data-image'));
 		clickedDotSlide.removeAttribute('hidden');
+		clickedDotImage.removeAttribute('hidden');
 	  clickedDot.setAttribute('aria-selected', "true")
 	})
 
