3d66308603127e27b32c15a19a7743e148961f41
Trapti Rahangdale <imtrapti1410@gmail.com>
2021-11-25 11:46:54 +0530

    migration to latest astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    //site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,         // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
@@ -13,4 +19,9 @@ export default {
     // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
   renderers: [],
+  vite: {
+    resolve: {
+      alias: [ {find: '$', replacement: path.resolve(__dirname, './src')} ]
+    }
+  }
 };
